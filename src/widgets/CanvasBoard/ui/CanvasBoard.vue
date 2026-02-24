961d9d2d5b9cd7bd4c05f1092a1df19b845b05e7
Alex Rezn <alexrezn45@yandex.ru>
2026-02-24 19:15:34 +0500

    refactor: add stickerBlock to CanvasBoard

--- src/widgets/CanvasBoard/ui/CanvasBoard.vue
+++ src/widgets/CanvasBoard/ui/CanvasBoard.vue
@@ -5,16 +5,38 @@
       :style="{ width: CANVAS_W + 'px', height: CANVAS_H + 'px' }"
     >
       <ProcessBlock v-for="proc in processes" :key="proc.id" :process="proc" />
+      <StickerBlock
+        v-for="sticker in stickersStore.stickers"
+        :key="sticker.id"
+        :sticker="sticker"
+        :isDragging="draggingId === sticker.id"
+        @sticker:updateText="updateStickerText"
+        @sticker:delete="removeSticker"
+        @sticker:resizeStart="onStickerResize"
+        @sticker:dragStart="onDragStart"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ProcessBlock } from '@/widgets';
+import { ProcessBlock, StickerBlock } from '@/widgets';
 import { processes } from '@/shared';
+import { useStickerStore } from '@/entities';
 
 const CANVAS_W = 1200;
 const CANVAS_H = 800;
+
+const stickersStore = useStickerStore();
+const { updateStickerText, removeSticker } = stickersStore;
+
+const draggingId = 't';
+const onStickerResize = () => {
+  console.log('onStickerResize');
+};
+const onDragStart = () => {
+  console.log('onDragStart');
+};
 </script>
 
 <style module lang="scss">
