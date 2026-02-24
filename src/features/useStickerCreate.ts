dfa12912f1be370c066766d426b7d8702cec1e33
Alex Rezn <alexrezn45@yandex.ru>
2026-02-24 16:56:34 +0500

    feat: useStickerCreate

---
+++ src/features/useStickerCreate.ts
@@ -0,0 +1,13 @@
+import { useStickerStore } from '@/entities';
+
+const DEFAULT_X = 580;
+const DEFAULT_Y = 380;
+
+export function useStickerCreate() {
+  const stickerStore = useStickerStore();
+
+  const createSticker = (x = DEFAULT_X, y = DEFAULT_Y) =>
+    stickerStore.addSticker(x, y);
+
+  return { createSticker };
+}
